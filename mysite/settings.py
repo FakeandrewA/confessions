a41866a7e33ff904a824cb6f1a9f09a425215f49
[Andrew A] <[andrewgtc2005@gmail.com]>
2025-06-30 20:42:41 +0530

    added Pydantic_settings for database credentials and migrated and made changes in the database

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from config import details
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,7 +75,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": details.database_name,
+        "USER":details.database_username,
+        "PASSWORD":details.database_password,
+        "HOST":details.database_hostname,
+        "PORT":details.database_port
     }
 }
 
